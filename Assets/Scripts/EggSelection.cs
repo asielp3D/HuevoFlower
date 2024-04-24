e3a5333191552eaf18239de79cca885a0a7bf473
asielp3D <asielmasespacio@gmail.com>
2024-04-24 17:04:10 +0200

    aa

---
+++ Assets/Scripts/EggSelection.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EggSelection : MonoBehaviour
+{
+    public string eggName;
+
+    void OnMouseDown()
+    {
+        PlayerPrefs.SetString("SelectedEgg", eggName);
+        SceneManager.LoadScene("huevo");
+    }
+}
---
+++ Assets/Scripts/EggSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggSpawner : MonoBehaviour
+{
+    public GameObject eggPrefab1;
+    public GameObject eggPrefab2;
+    public Transform spawnPosition;
+
+    void Start()
+    {
+        string selectedEgg = PlayerPrefs.GetString("SelectedEgg");
+
+        if (selectedEgg == "huevo_azul")
+        {
+            Instantiate(eggPrefab1, spawnPosition.position, Quaternion.identity);
+        }
+        else if (selectedEgg == "huevo_rojo")
+        {
+            Instantiate(eggPrefab2, spawnPosition.position, Quaternion.identity);
+        }
+    }
+}
