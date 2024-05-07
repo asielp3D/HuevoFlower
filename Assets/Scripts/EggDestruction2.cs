54f548d832a51d0406e99cf3f1e4a787b1128494
asielp3D <asielmasespacio@gmail.com>
2024-05-07 17:58:32 +0200

    a

---
+++ Assets/Scripts/EggDestruction2.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EggDestruction2 : MonoBehaviour
+{
+    [SerializeField] private float explosiveForce = 100f;
+    [SerializeField] private float explosiveRadius = 2f;
+    [SerializeField] private float pieceFadeSpeed = 0.25f;
+    [SerializeField] private float pieceDestroyDelay = 5f;
+    [SerializeField] private float pieceSleepCheckDelay = 0.5f;
+
+    void Start()
+    {
+        StartCoroutine(ExplodeCoroutine());
+    }
+
+    public IEnumerator ExplodeCoroutine()
+    {
+        Rigidbody[] pieces = GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody body in pieces)
+        {
+            body.AddExplosionForce(explosiveForce, transform.position, explosiveRadius);
+        }
+
+        yield return FadeOutRigidBodies(pieces);
+    }
+
+    private IEnumerator FadeOutRigidBodies(Rigidbody[] pieces)
+    {
+        WaitForSeconds wait = new WaitForSeconds(pieceSleepCheckDelay);
+        int activeRigidBodies = pieces.Length;
+
+        while (activeRigidBodies > 0)
+        {
+            yield return wait;
+
+            foreach (Rigidbody body in pieces)
+            {
+                if (body.IsSleeping())
+                {
+                    activeRigidBodies--;
+                }
+            }
+
+            yield return new WaitForSeconds(pieceDestroyDelay);
+
+            Renderer[] renderers = Array.ConvertAll(pieces, body => body.GetComponent<Renderer>());
+
+            foreach (Rigidbody body in pieces)
+            {
+                Destroy(body.GetComponent<Collider>());
+                Destroy(gameObject);
+            }
+
+            float time = 0f;
+            while (time < 1f)
+            {
+                float step = Time.deltaTime * pieceFadeSpeed;
+
+                foreach (Renderer renderer in renderers)
+                {
+                    renderer.transform.Translate(Vector3.down * (step / renderer.bounds.size.y), Space.World);
+                }
+
+                time += step;
+                yield return null;
+            }
+
+            foreach (Renderer renderer in renderers)
+            {
+                Destroy(renderer.gameObject);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Huevo2.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Huevo2 : MonoBehaviour
+{
+    [SerializeField]private GameObject _brokenEggPrefab;
+
+    [SerializeField]private float eggHealth = 10;
+    [SerializeField]private Slider healthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //healthSlider.maxValue=eggHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && eggHealth >= 0) 
+        {
+          EggHit();
+        }  
+    }
+
+    void EggHit()
+    {
+        if(eggHealth > 0) 
+        {
+            eggHealth -= 1; 
+            healthSlider.value -=1;
+
+            if(eggHealth == 0)
+            {
+                Instantiate(_brokenEggPrefab, transform.position, transform.rotation);
+                Destroy(this.gameObject);
+            }
+        }  
+    }
+}
+
