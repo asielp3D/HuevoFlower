f6f13a381a3361d2f5f9f1928c15436155254b37
rgo555 <asielmasespacio@gmail.com>
2024-05-20 12:13:15 +0200

    a

--- Assets/Scripts/EggSpawner.cs
+++ Assets/Scripts/EggSpawner.cs
@@ -4,21 +4,41 @@ using UnityEngine;
 
 public class EggSpawner : MonoBehaviour
 {
-    public GameObject eggPrefab1;
-    public GameObject eggPrefab2;
+    public GameObject eggPrefabLago1;
+    public GameObject eggPrefabLago2;
+    public GameObject eggPrefabCamp1;
+    public GameObject eggPrefabCamp2;
+    public GameObject eggPrefabNieve1;
+    public GameObject eggPrefabNieve2;
     public Transform spawnPosition;
 
     void Start()
     {
         string selectedEgg = PlayerPrefs.GetString("SelectedEgg");
 
-        if (selectedEgg == "huevo_azul")
+        if (selectedEgg == "lago_1")
         {
-            Instantiate(eggPrefab1, spawnPosition.position, Quaternion.identity);
+            Instantiate(eggPrefabLago1, spawnPosition.position, Quaternion.identity);
         }
-        else if (selectedEgg == "huevo_rojo")
+        else if (selectedEgg == "lago_2")
         {
-            Instantiate(eggPrefab2, spawnPosition.position, Quaternion.identity);
+            Instantiate(eggPrefabLago2, spawnPosition.position, Quaternion.identity);
+        }
+        else if (selectedEgg == "camp_1")
+        {
+            Instantiate(eggPrefabCamp2, spawnPosition.position, Quaternion.identity);
+        }
+        else if (selectedEgg == "camp_2")
+        {
+            Instantiate(eggPrefabCamp2, spawnPosition.position, Quaternion.identity);
+        }
+        else if (selectedEgg == "nieve_1")
+        {
+            Instantiate(eggPrefabNieve2, spawnPosition.position, Quaternion.identity);
+        }
+        else if (selectedEgg == "nieve_2")
+        {
+            Instantiate(eggPrefabNieve2, spawnPosition.position, Quaternion.identity);
         }
     }
 }
