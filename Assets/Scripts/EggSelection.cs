1ae51fe8cedc715ce3cd0b3864248508ecb6c227
ainetta12 <ainasanzg@gmail.com>
2024-05-28 10:28:57 +0200

    a

--- Assets/Scripts/EggSelection.cs
+++ Assets/Scripts/EggSelection.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,5 +12,6 @@ public class EggSelection : MonoBehaviour
     {
         PlayerPrefs.SetString("SelectedEgg", eggName);
         SceneManager.LoadScene(3);
+        Destroy(this);
     }
 }
